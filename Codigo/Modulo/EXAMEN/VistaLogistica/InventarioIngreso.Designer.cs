8360962b27362098cc8802e1935c4d3273420344
unknown <jmmarroquinr00@gmail.com>
2022-11-15 11:16:58 -0600

    nuevco

---
+++ Codigo/Modulo/EXAMEN/VistaLogistica/InventarioIngreso.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace VistaLogistica
+{
+    partial class InventarioIngreso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador1 = new NavegadorVista.Navegador();
+            this.dtgProducto = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtDesc = new System.Windows.Forms.TextBox();
+            this.TxtCodigoRef = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProducto)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(18, 4);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(5);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(765, 187);
+            this.navegador1.TabIndex = 70;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // dtgProducto
+            // 
+            this.dtgProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProducto.Location = new System.Drawing.Point(12, 414);
+            this.dtgProducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgProducto.Name = "dtgProducto";
+            this.dtgProducto.RowHeadersWidth = 51;
+            this.dtgProducto.RowTemplate.Height = 24;
+            this.dtgProducto.Size = new System.Drawing.Size(776, 278);
+            this.dtgProducto.TabIndex = 69;
+            this.dtgProducto.Tag = "tbl_inventario";
+            this.dtgProducto.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProducto_CellContentClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBuscar);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.TxtDesc);
+            this.groupBox2.Controls.Add(this.TxtCodigoRef);
+            this.groupBox2.Location = new System.Drawing.Point(37, 200);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(713, 170);
+            this.groupBox2.TabIndex = 68;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(319, 37);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(306, 22);
+            this.txtBuscar.TabIndex = 1;
+            this.txtBuscar.Tag = "pk_inventario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo del Ingreso";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label5.Location = new System.Drawing.Point(23, 108);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Existencias";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(23, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 24);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Codigo Producto";
+            // 
+            // TxtDesc
+            // 
+            this.TxtDesc.Location = new System.Drawing.Point(319, 111);
+            this.TxtDesc.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtDesc.Name = "TxtDesc";
+            this.TxtDesc.Size = new System.Drawing.Size(306, 22);
+            this.TxtDesc.TabIndex = 42;
+            this.TxtDesc.Tag = "existencias";
+            // 
+            // TxtCodigoRef
+            // 
+            this.TxtCodigoRef.Location = new System.Drawing.Point(319, 70);
+            this.TxtCodigoRef.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtCodigoRef.Name = "TxtCodigoRef";
+            this.TxtCodigoRef.Size = new System.Drawing.Size(306, 22);
+            this.TxtCodigoRef.TabIndex = 39;
+            this.TxtCodigoRef.Tag = "fk_codigo_producto";
+            // 
+            // InventarioIngreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 740);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.dtgProducto);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "InventarioIngreso";
+            this.Text = "InventarioIngreso";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProducto)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NavegadorVista.Navegador navegador1;
+        private System.Windows.Forms.DataGridView dtgProducto;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtDesc;
+        private System.Windows.Forms.TextBox TxtCodigoRef;
+    }
+}
---
+++ Codigo/Modulo/EXAMEN/VistaLogistica/Producto.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace VistaLogistica
+{
+    partial class Producto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtLinea = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtDesc = new System.Windows.Forms.TextBox();
+            this.TxtCodigoRef = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtgProducto = new System.Windows.Forms.DataGridView();
+            this.navegador1 = new NavegadorVista.Navegador();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProducto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(319, 37);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(306, 22);
+            this.txtBuscar.TabIndex = 1;
+            this.txtBuscar.Tag = "pk_codigo_producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo del Producto";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBuscar);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.TxtLinea);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.TxtDesc);
+            this.groupBox2.Controls.Add(this.TxtCodigoRef);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(43, 205);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(713, 221);
+            this.groupBox2.TabIndex = 65;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label5.Location = new System.Drawing.Point(23, 108);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 24);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Descripcion";
+            // 
+            // TxtLinea
+            // 
+            this.TxtLinea.Location = new System.Drawing.Point(319, 143);
+            this.TxtLinea.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtLinea.Name = "TxtLinea";
+            this.TxtLinea.Size = new System.Drawing.Size(306, 22);
+            this.TxtLinea.TabIndex = 46;
+            this.TxtLinea.Tag = "Precio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(23, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Nombre";
+            // 
+            // TxtDesc
+            // 
+            this.TxtDesc.Location = new System.Drawing.Point(319, 111);
+            this.TxtDesc.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtDesc.Name = "TxtDesc";
+            this.TxtDesc.Size = new System.Drawing.Size(306, 22);
+            this.TxtDesc.TabIndex = 42;
+            this.TxtDesc.Tag = "Descripcion";
+            // 
+            // TxtCodigoRef
+            // 
+            this.TxtCodigoRef.Location = new System.Drawing.Point(319, 70);
+            this.TxtCodigoRef.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtCodigoRef.Name = "TxtCodigoRef";
+            this.TxtCodigoRef.Size = new System.Drawing.Size(306, 22);
+            this.TxtCodigoRef.TabIndex = 39;
+            this.TxtCodigoRef.Tag = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 142);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Precio";
+            // 
+            // dtgProducto
+            // 
+            this.dtgProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProducto.Location = new System.Drawing.Point(17, 451);
+            this.dtgProducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgProducto.Name = "dtgProducto";
+            this.dtgProducto.RowHeadersWidth = 51;
+            this.dtgProducto.RowTemplate.Height = 24;
+            this.dtgProducto.Size = new System.Drawing.Size(776, 278);
+            this.dtgProducto.TabIndex = 66;
+            this.dtgProducto.Tag = "tbl_productos";
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(24, 9);
+            this.navegador1.Margin = new System.Windows.Forms.Padding(5);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(765, 187);
+            this.navegador1.TabIndex = 67;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // Producto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 760);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.dtgProducto);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Producto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Producto";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProducto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtLinea;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtDesc;
+        private System.Windows.Forms.TextBox TxtCodigoRef;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dtgProducto;
+        private NavegadorVista.Navegador navegador1;
+    }
+}
